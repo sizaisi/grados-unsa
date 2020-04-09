900e6a4bf2d49a85b201a4bc76c45b766e81f054
Renzo <rsiza@unsa.edu.pe>
2020-04-09 17:38:43 -0500

    llenado de base de datos y configuracion de expedientes

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Expedientes</b-nav-item>
+          <b-nav-item to="/inicio">Expedientes</b-nav-item>            
           <b-nav-item href="#">Reportes</b-nav-item>          
           <!-- Navbar dropdowns -->
           <b-nav-item-dropdown text="Mantenimiento" left>
