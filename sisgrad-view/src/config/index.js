228f5a47689bbb41315d8474efeb01a98fb02caf
renzosiza7 <rsiza@unsa.edu.pe>
2020-08-20 20:49:39 -0500

    Cambios links para backend

--- sisgrad-view/src/config/index.js
+++ sisgrad-view/src/config/index.js
@@ -1,34 +1,53 @@
 const config = {
-    /***** SISACAD *****/
-    //API_URL: '//190.119.145.150:8023/sisacad/graduacion/grados-unsa/sisgrad-ws',
+  /***** SISACAD *****/
+  //API_URL: '//190.119.145.150:8023/sisacad/graduacion/grados-unsa/sisgrad-ws',
 
-    /***** SISDOCE *****/
-    //API_URL: '//190.119.145.150:8022/sisacad/sisdoce/graduacion/grados-unsa/sisgrad-ws',
+  /***** SISDOCE *****/
+  API_URL:
+    '//190.119.145.150:8022/sisacad/sisdoce/graduacion/grados-unsa/sisgrad-ws',
 
-    /***** LOCAL *****/
-    API_URL: '//localhost/grados-unsa/sisgrad-ws',    
-    
-    //para asignacion de colores a los movimientos o rutas de salida de los procedimientos
-    color_acciones: { enviar:"success", derivar:"success", 
-                      devolver:"danger", denegar:"danger",
-                      observar:"warning", rechazar:"danger", 
-                      aceptar:"success", aprobar:"success", 
-                      anular: "danger", finalizar: "primary"
-                    },
-    //para asignacion de colores a los estados del listado de expedientes
-    color_estados: { enviado:"success", derivado:"success", 
-                      devuelto:"danger", denegado:"danger",
-                      observado:"warning", rechazado:"danger", 
-                      aceptado:"success", aprobado:"success", 
-                      anulado: "danger", finalizado: "primary"
-                    },
-    //para movimientos de ingreso a un procedimiento
-    estados: { enviar:"enviado", derivar:"derivado", 
-                devolver:"devuelto", denegar:"denegado",
-                observar:"observado", rechazar:"rechazado", 
-                aceptar:"aceptado", aprobar:"aprobado", 
-                anular: "anulado", finalizar: "finalizado"
-              },
-}
+  /***** LOCAL *****/
+  //API_URL: '//localhost/grados-unsa/sisgrad-ws',
 
-export default config
+  //para asignacion de colores a los movimientos o rutas de salida de los procedimientos
+  color_acciones: {
+    enviar: 'success',
+    derivar: 'success',
+    devolver: 'danger',
+    denegar: 'danger',
+    observar: 'warning',
+    rechazar: 'danger',
+    aceptar: 'success',
+    aprobar: 'success',
+    anular: 'danger',
+    finalizar: 'primary',
+  },
+  //para asignacion de colores a los estados del listado de expedientes
+  color_estados: {
+    enviado: 'success',
+    derivado: 'success',
+    devuelto: 'danger',
+    denegado: 'danger',
+    observado: 'warning',
+    rechazado: 'danger',
+    aceptado: 'success',
+    aprobado: 'success',
+    anulado: 'danger',
+    finalizado: 'primary',
+  },
+  //para movimientos de ingreso a un procedimiento
+  estados: {
+    enviar: 'enviado',
+    derivar: 'derivado',
+    devolver: 'devuelto',
+    denegar: 'denegado',
+    observar: 'observado',
+    rechazar: 'rechazado',
+    aceptar: 'aceptado',
+    aprobar: 'aprobado',
+    anular: 'anulado',
+    finalizar: 'finalizado',
+  },
+};
+
+export default config;
--- sisgrad-view/vue.config.js
+++ sisgrad-view/vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  /*publicPath: process.env.NODE_ENV === 'production'
-                    ? '/sisacad/graduacion/grados-unsa/administrativo/'
-                    : '/'*/
+  /*publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/sisacad/graduacion/grados-unsa/administrativo/'
+      : '/',*/
   publicPath:
     process.env.NODE_ENV === 'production'
       ? '/sisacad/sisdoce/graduacion/grados-unsa/docente/'
